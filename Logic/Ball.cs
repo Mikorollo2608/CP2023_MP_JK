551e126697ec9966554edb0af7cf812956b89c58
Mikorollo2608 <michal.p2608@wp.pl>
2023-04-01 15:43:55 +0200

    Added radius, velocities fields and move() method for the Ball class

--- Logic/Ball.cs
+++ Logic/Ball.cs
@@ -1,19 +1,71 @@
-﻿
-namespace Logic
+﻿namespace Logic
 {
     public class Ball
     {
         private int x;
         private int y;
+        private int velocityX;
+        private int velocityY;
 
-        public  Ball(int x, int y)
+        public static readonly int radius = 5;
+
+        public  Ball(int x, int y, int velocityX, int velocityY)
         {
             this.x = x;
             this.y = y;
+            this.velocityX = velocityX;
+            this.velocityY = velocityY;
         }
 
         public int getX() { return x; }
         public int getY() { return y; }
+        public int getVelocityX() { return velocityX; }
+        public int getVelocityY() { return velocityY; }
 
+        public void setVelocityX(int v) { velocityX = v; }
+        public void setVelocityY(int v) { velocityY = v; }
+
+        public void move(int borderX, int borderY)
+        {
+            if ( (borderX >= (x + radius)) && (borderY >= (y + radius)) )
+            {
+                if ( (x + velocityX + radius) >= 0 && ((x + velocityX + radius) <= borderX) )
+                {
+                    x += velocityX;
+                }
+                else 
+                {
+                    if ( (x + velocityX + radius) >= 0 )
+                    {
+                        x = borderX - radius;
+                    }
+                    else
+                    {
+                        x = 0 + radius;
+                    }
+                        
+                    velocityX *= -1;
+                }
+
+                if ( (y + velocityY + radius) >= 0 && ((y + velocityY + radius) <= borderY) )
+                {
+                    y += velocityY;
+                }
+                else
+                {
+                    if ( (y + velocityY + radius) >= 0 )
+                    {
+                        y = borderY - radius;
+                    }
+                    else
+                    {
+                        y = 0 + radius;
+                    }
+
+                    velocityY *= -1;
+                }
+
+            }
+        }
     }
 }
--- TestLogic/UnitTest1.cs
+++ TestLogic/UnitTest1.cs
@@ -6,14 +6,55 @@ namespace TestLogic
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetters()
         {
-            Ball ball = new Ball(5, 8);
+            Ball ball = new Ball(5, 8, 1, 2);
 
             Assert.IsNotNull(ball);
             Assert.AreEqual(5, ball.getX());
             Assert.AreEqual(8, ball.getY());
+            Assert.AreEqual(1, ball.getVelocityX());
+            Assert.AreEqual(2, ball.getVelocityY());
 
+            Assert.AreEqual(5, Ball.radius);
+        }
+
+        [TestMethod]
+        public void TestSetters()
+        {
+            Ball ball = new Ball(1, 1, 5, 6);
+
+            Assert.AreEqual(5, ball.getVelocityX());
+            Assert.AreEqual(6, ball.getVelocityY());
+
+            ball.setVelocityX(10);
+            ball.setVelocityY(13);
+
+            Assert.AreEqual(10, ball.getVelocityX());
+            Assert.AreEqual(13, ball.getVelocityY());
+        }
+
+        [TestMethod]
+        public void TestMove()
+        {
+            Ball ball = new Ball(12, 15, 3, 2);
+            int borderX = 100;
+            int borderY = 80;
+
+            Assert.AreEqual(12, ball.getX());
+            Assert.AreEqual(15, ball.getY());
+            
+            //Test normal move
+            ball.move(borderX, borderY);
+            Assert.AreEqual(15, ball.getX());
+            Assert.AreEqual(17, ball.getY());
+
+            //Test move outside one border
+            ball.setVelocityX(100);
+            ball.move(borderX, borderY);
+            Assert.AreEqual(95, ball.getX());
+            Assert.AreEqual(-100, ball.getVelocityX());
+            Assert.AreEqual(19, ball.getY());
         }
     }
 }
